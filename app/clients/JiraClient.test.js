ae92205b967b91c16bcf042f0d5dc47d2d084577
Felipe Ramirez <feliperamirezg@gmail.com>
2019-01-03 11:40:08 -0300

    adds search jira call

--- app/clients/JiraClient.test.js
+++ app/clients/JiraClient.test.js
@@ -3,5 +3,13 @@ import JiraClient from './JiraClient'
 it('returns the Jira API to explore it', () => {
     const jiraApi = JiraClient.getJiraAPI()
     expect(jiraApi).toBeDefined()
-    console.log(jiraApi)
+
+    const results = jiraApi.searchJira()
+    .then(results => {
+        console.log(results)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
 })
