a7f145a72656bb7491d01fba53889508df9bd7db
Alejandro Labra <alejandro.labra@thoughtworks.com>
2019-01-14 14:32:17 -0300

    refactor project.service.test, setup spy for projectRepository

--- app/services/project.service.test.js
+++ app/services/project.service.test.js
@@ -4,8 +4,14 @@ import ProjectRepository from '../repositories/project.repository';
 
 describe('Create new Project', () => {
     it('given a new project name, then create new project', async () => {
-        var project = new Project('project-name', 'jira')
-        const projectRepository = new ProjectRepository()
+        let project = new Project('project-name', 'jira')
+        let projectRepository = new ProjectRepository()
+        jest.spyOn(projectRepository, "find").mockImplementation(async () => null);
+        jest.spyOn(projectRepository, "save").mockImplementation(async () => {
+            project.id = 1 
+            return project
+        });
+
         const projectService = new ProjectService({projectRepository})
 
         project = await projectService.save(project)
@@ -18,8 +24,8 @@ describe('Create new Project', () => {
 
     it('given a exists project name when create, then throw Exits exception', async () => {
         var project = new Project('project-name', 'jira')
-        const projectRepository = new ProjectRepository()
-        await projectRepository.save(project)
+        let projectRepository = new ProjectRepository()
+        jest.spyOn(projectRepository, "find").mockImplementation(async (name) => {return new Project(name, '')});
         
         const projectService = new ProjectService({projectRepository})
         let error;
