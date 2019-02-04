befa449051cd6e91075a32425e51b0deb42ea163
Alejandro Labra <alejandro.labra@thoughtworks.com>
2019-02-04 11:19:02 -0300

    Refactor test

--- app/routes/throughput.validate.route.test.js
+++ app/routes/throughput.validate.route.test.js
@@ -4,22 +4,21 @@ import ThroughputService from '../services/throughput.sevice'
 import ThroughputRoute from './throughput.route'
 
 jest.mock('express-validator')
+let result
 
 describe('test validation definition', () => {
-    it('when call validate then return array with validations definitions', async () => {
+    beforeEach(() => {
         const throughputService = new ThroughputService({})
         const throughputRoute = new ThroughputRoute({ throughputService })
 
-        const result = throughputRoute.validate()
+        result = throughputRoute.validate()
+    })
+
+    it('when call validate then return array with validations definitions', async () => {
         expect(result).toBeDefined()
     })
 
     it('start field is not empty with message "it is mandatory" ', () => {
-        const throughputService = new ThroughputService({})
-        const throughputRoute = new ThroughputRoute({ throughputService })
-
-        const result = throughputRoute.validate()
-
         const startValidations = _.filter(result, (check) => check._context.fields[0] === 'start')
         expect(startValidations.find((val) =>
             val._context.validators[0].message === 'it is mandatory' &&
@@ -28,11 +27,6 @@ describe('test validation definition', () => {
     })
 
     it('end field is not empty with message "it is mandatory" ', () => {
-        const throughputService = new ThroughputService({})
-        const throughputRoute = new ThroughputRoute({ throughputService })
-
-        const result = throughputRoute.validate()
-
         const endValidations = _.filter(result, (check) => check._context.fields[0] === 'end')
         expect(endValidations.find((val) =>
             val._context.validators[0].message === 'it is mandatory' &&
@@ -41,11 +35,6 @@ describe('test validation definition', () => {
     })
 
     it('periodTime field is not empty with message "it is mandatory" ', () => {
-        const throughputService = new ThroughputService({})
-        const throughputRoute = new ThroughputRoute({ throughputService })
-
-        const result = throughputRoute.validate()
-
         const periodTimeValidations = _.filter(result, (check) => check._context.fields[0] === 'periodTime')
         expect(periodTimeValidations.find((val) =>
             val._context.validators[0].message === 'it is mandatory' &&
@@ -54,31 +43,16 @@ describe('test validation definition', () => {
     })
 
     it('periodTime field is in day or week values with message "must be a "day" or "week" values" ', () => {
-        const throughputService = new ThroughputService({})
-        const throughputRoute = new ThroughputRoute({ throughputService })
-
-        const result = throughputRoute.validate()
-
         const periodTimeValidations = _.filter(result, (check) => check._context.fields[0] === 'periodTime')
         expect(periodTimeValidations.find((val) => val._context.validators[0].message === 'must be a "day" or "week" values' && val._context.validators[0].validator === isIn)).toBeTruthy()
     })
 
     it('start field is valid format with message "it is invalid format" ', () => {
-        const throughputService = new ThroughputService({})
-        const throughputRoute = new ThroughputRoute({ throughputService })
-
-        const result = throughputRoute.validate()
-
         const startValidations = _.filter(result, (check) => check._context.fields[0] === 'start')
         expect(startValidations.find((val) => val._context.validators[0].message === 'it is invalid format' && val._context.validators[0].custom === true)).toBeTruthy()
     })
 
     it('end field is valid format with message "it is invalid format" ', () => {
-        const throughputService = new ThroughputService({})
-        const throughputRoute = new ThroughputRoute({ throughputService })
-
-        const result = throughputRoute.validate()
-
         const endValidations = _.filter(result, (check) => check._context.fields[0] === 'end')
         expect(endValidations.find((val) => val._context.validators[0].message === 'it is invalid format' && val._context.validators[0].custom === true)).toBeTruthy()
     })
