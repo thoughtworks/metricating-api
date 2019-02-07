1a096a15f6c5f06be24a88e179ce33715bc39d1d
Alejandro Labra <alejandro.labra@thoughtworks.com>
2019-02-06 23:06:00 -0300

    refactor creation projectRepository

--- app/repositories/project.repository.test.js
+++ app/repositories/project.repository.test.js
@@ -1,22 +1,18 @@
 import ProjectRepository from './project.repository'
 
-it('projectRepository is defined', async () => {
-    const projectRepository = new ProjectRepository()
+const projectRepository = new ProjectRepository()
 
+it('projectRepository is defined', async () => {
     expect(projectRepository).toBeDefined()
 })
 
 it('find method is defined', async () => {
-    const projectRepository = new ProjectRepository()
-
     const result = projectRepository.find()
 
     expect(result).toBeDefined()
 })
 
 it('save method is defined', async () => {
-    const projectRepository = new ProjectRepository()
-
     const result = projectRepository.save()
 
     expect(result).toBeDefined()
