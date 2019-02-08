3cd5c1ee9c8563a1a714a96869b6cd3174f45e56
Alejandro Labra <alejandro.labra@thoughtworks.com>
2019-02-08 18:49:47 -0300

    #1 Fix UnhandledPromiseRejectionWarning in test

--- app/routes/validations/date.validation.test.js
+++ app/routes/validations/date.validation.test.js
@@ -1,13 +1,15 @@
 import DateValidate from './date.validation'
 
-it('when date format is invalid then return reject promise', () => {
-    const result = DateValidate.validate('2018W89')
-
-    expect(result).toMatchObject(Promise.reject('it is invalid format'))
+it('when date format is invalid then return reject promise', async () => {
+    DateValidate.validate('2018W89').catch((error) => {
+        expect(error).toBe('it is invalid format')
+    })
 })
 
 it('when date format is valid, then return promise resolve.', () => {
-    const result = DateValidate.validate('2018W50')
-
-    expect(result).toMatchObject(Promise.resolve())
+    DateValidate.validate('2018W50').then((result) => {
+        expect(result).toBe(undefined)
+    }).catch((error) => {
+        expect(error).not.toBeCalled()
+    })
 })
