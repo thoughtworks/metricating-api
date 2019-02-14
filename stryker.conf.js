1382830fe9215e0e341c6d23cd90e1c05eeab30c
Alejandro Labra <alejandro.labra@thoughtworks.com>
2019-02-14 19:07:53 -0300

    add html report for stryker.

--- stryker.conf.js
+++ stryker.conf.js
@@ -2,7 +2,7 @@ module.exports = function(config) {
     config.set({
         mutator: 'javascript',
         packageManager: 'npm',
-        reporters: ['clear-text', 'progress'],
+        reporters: ['clear-text', 'progress', 'html'],
         testRunner: 'jest',
         transpilers: [],
         coverageAnalysis: 'off',
