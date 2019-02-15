a07f13dd47d8a8ddfa32c3937c73902c29c17627
Alejandro Labra <alejandro.labra@thoughtworks.com>
2019-02-15 00:50:32 -0300

    Fix of unit tests because mutations testing.

--- app/routes/validations/date.validation.test.js
+++ app/routes/validations/date.validation.test.js
@@ -1,15 +1,25 @@
+import { fail } from 'assert'
+import { when } from 'jest-when'
+import moment from 'moment'
 import DateValidate from './date.validation'
 
+jest.mock('moment')
+
 it('when date format is invalid then return reject promise', async () => {
-    DateValidate.validate('2018W89').catch((error) => {
+    when(moment).calledWith('2018W89').mockImplementation({ isValid: jest.fn().mockReturnValue(false) })
+    try {
+        await DateValidate.validate('2018W89')
+        fail()
+    } catch (error) {
         expect(error).toBe('it is invalid format')
-    })
+    }
 })
 
-it('when date format is valid, then return promise resolve.', () => {
-    DateValidate.validate('2018W50').then((result) => {
-        expect(result).toBe(undefined)
-    }).catch((error) => {
-        expect(error).not.toBeCalled()
-    })
+it('when date format is valid, then return promise resolve.', async () => {
+    when(moment).calledWith('2018W50').mockImplementation({ isValid: jest.fn().mockReturnValue(true) })
+    try {
+        await DateValidate.validate('2018W50')
+    } catch (error) {
+        fail()
+    }
 })
