b5ac5fa694597f1b0da488a1e2bbc99d53e80b96
Alejandro Labra <alejandro.labra@thoughtworks.com>
2019-02-15 14:25:52 -0300

    add cron stage for ci

---
+++ .travis_before.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+if [ "$1" == "cron" ]; then
+    curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+    chmod +x ./cc-test-reporter
+    ./cc-test-reporter before-build
+fi
